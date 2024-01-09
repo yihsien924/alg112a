90f4a69405ae8e1f4b020f1fb1ad4789e01da902
林羿嫻 <99934978+yihsien924@users.noreply.github.com>
2024-01-10 00:23:01 +0800

    Update hw3.py

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -4,7 +4,7 @@ def generate_permutations(input_list):
     all_permutations = list(permutations(input_list))
     return all_permutations
 
-# 測試程式碼
+#測試程式碼
 user_input = input("輸入（以空格分隔元素）：")
 input_list = user_input.split()
 all_permutations = generate_permutations(input_list)
